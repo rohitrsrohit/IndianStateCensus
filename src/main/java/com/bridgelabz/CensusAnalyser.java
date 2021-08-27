ca073b8d3890b421b7b32105fad7d35847584961
Rohit Patro <rohitpatro6@gmail.com>
2021-08-27 22:31:04 +0530

    ADDED:The Indian State Census Report Added

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz;
-
-public class CensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Indian State Census Analyser");
-    }
-}
